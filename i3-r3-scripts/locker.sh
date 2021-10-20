23928d7156bbd1e3f043ac3d5a02103373845b15
rav3ndust <35274771+rav3ndust@users.noreply.github.com>
2021-10-20 04:21:09 -0500

    Add files via upload

---
+++ i3-r3-scripts/locker.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+###
+#locker
+# locks the i3-r3 session for you
+
+i3lock-fancy
+notify-send "i3r3 Locker - a message from $HOSTNAME" "Welcome back to your computer, $USER. It is good to see you again!"
+exit
