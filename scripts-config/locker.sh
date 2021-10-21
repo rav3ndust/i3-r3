378c120e84426b2c065617854b2d7bcac145c635
rav3ndust <35274771+rav3ndust@users.noreply.github.com>
2021-10-21 01:26:18 -0500

    Add files via upload
    adding new configs and scripts

---
+++ scripts-config/locker.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+###
+#locker
+# locks the nightshadeWM session for you
+
+i3lock-fancy
+notify-send "nightshadeWM Locker - a message from $HOSTNAME" "Welcome back to your computer, $USER. It is good to see you again!"
+exit
