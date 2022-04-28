8c17aa951f786103f8e586cff4d4dcb07ef2bcc5
rav3ndust <35274771+rav3ndust@users.noreply.github.com>
2022-04-28 16:10:30 -0500

    updates to installer.sh
    updates to installer.sh including vimrc and some other stuff, including setting def wallpaper and finishing up the script. about to test this installation script out in a VM, will update when finished.

--- scripts-config/installer.sh
+++ scripts-config/installer.sh
@@ -28,13 +28,15 @@ i3STATUS_LOCATION="/etc/i3status.conf"
 WIRED_i3STATUS_CONFIG="wiredWM/scripts-config/configs/i3status-config"
 CONKY_CONFIG_LOCATION="etc/conky/conky.conf"
 WIRED_CONKY_CONFIG="wiredWM/scripts-config/configs/conky.conf"
-DUNSTRC_CONFIG=""
+DUNSTRC_CONFIG_LOCATION_1="/etc/dunst/dunstrc"
+DUNSTRC_CONFIG_LOCATION_2="~/.config/dunst/dunstrc"
 WIRED_DUNSTRC="wiredWM/scripts-config/configs/dunstrc" 
 VIM_CONFIG="~/.vimrc"
+WIRED_VIM_CONFIG="~/wiredWM/scripts-config/configs/vimrc"
 i3_INSTALL=$(sudo apt install i3 i3lock-fancy -y)
-WIREDWM_CLONE=$(git clone https://github.com/rav3ndust/wiredWM)
 UPDATER=$(sudo apt update)
-WIRED_PACKS=$(sudo apt install nitrogen arandr volumeicon-alsa flameshot kitty stterm surf conky suckless-tools vim -y)
+WIRED_PACKS=$(sudo apt install nitrogen arandr volumeicon-alsa flameshot kitty stterm surf conky suckless-tools vim -y) 
+DEF_WP_LOCATION="~/wiredWM/wp/nExt.png"
 ################################
 function sleepy() {	# this function sleeps sys 1 sec
 	sleep 1
@@ -53,11 +55,46 @@ sleepy
 echo "Downloading i3 and its components. Please be patient."
 $i3_INSTALL
 sleepy
-# grab wiredWM from the repository. 
-echo "Downloading wiredWM configurations..."
-$WIREDWM_CLONE
+# change dirs to /wiredWM/scripts-config/configs and begin applying configs to needed places.
+cd ~/wiredWM/scripts-config/configs
+# apply the i3-config file to /etc/i3/config and ~/.config/i3/config
+echo "Copying wiredWM configuration files..."
 sleepy
-echo "wiredWM repo cloned. Changing directories..."
+echo "Copying i3-config..."
+sudo cp -f $WIRED_i3_CONFIG $i3_CONFIG_1_LOCATION
+sudo cp -f $WIRED_i3_CONFIG $i3_CONFIG_2_LOCATION
+echo "Done! If needed, you can edit your configuration files at ~/.config/i3/config anytime." 
 sleepy
-# change dirs to /wiredWM and begin applying configs to needed places.
-cd wiredWM
+# apply the i3status-config file to /etc/i3status.conf
+echo "Copying i3status-config..."
+sudo cp -f $WIRED_i3STATUS_CONFIG $i3STATUS_LOCATION
+sleepy
+echo "i3status-config copied." && sleepy
+echo "You can edit it anytime at /etc/i3status.conf" && sleepy
+# apply the conky config to /etc/conky/conky.conf
+echo "Copying conky.conf..."
+sudo cp -f $WIRED_CONKY_CONFIG $CONKY_CONFIG_LOCATION
+sleepy
+echo "conky.conf copied." && sleepy
+echo "You can edit conky at /etc/conky/conky.conf anytime." && sleepy 
+# apply the dunstrc to /etc/dunst/dunstrc and ~/config.dunst/dunstrc
+echo "Copying dunstrc..."
+sudo mkdir /etc/dunst && sudo touch /etc/dunst/dunstrc
+sudo mkdir ~/.config/dunst && sudo touch ~/config/dunst/dunstrc
+sudo cp -f $WIRED_DUNSTRC $DUNSTRC_CONFIG_LOCATION_1
+sudo cp -f $WIRED_DUNSTRC $DUNSTRC_CONFIG_LOCATION_2
+echo "dunstrc copied." && sleepy
+# apply the vimrc to ~/.vimrc
+echo "Copying vim configs..."
+sudo cp -f $WIRED_VIM_CONFIG $VIM_CONFIG
+echo "Vim configs copied. You can change it at ~/.vimrc."
+# set the default background image.
+# we are going to use wp/nExt.png.
+echo "Setting default wallpaper..." && sleepy 
+nitrogen --set-auto --save $DEF_WP_LOCATION
+echo "Wallpaper saved. To change it, simply launch Nitrogen and choose whatever you would like." && sleepy 
+# all done 
+echo "wiredWM has been installed." && sleepy 
+echo "In order to log into it, please log out of your current X Session and log into 'i3', which you can now find in your desktop environment list in your login manager." && sleepy 
+notify-send "wiredWM installer" "wiredWM has been installed." 
+sleepy && exit 
