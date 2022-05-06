b0712605d0faa711e4ccd34fd7b93368d811abad
rav3ndust <35274771+rav3ndust@users.noreply.github.com>
2022-05-06 14:37:42 -0500

    add xautolock to packages
    support xautolock for automatically locking the screen after 10 mins of inactivity.

--- scripts-config/arch-install-demo/installer.sh
+++ scripts-config/arch-install-demo/installer.sh
@@ -30,6 +30,7 @@
 # - we use *nemo* as a graphical file browser.
 # - we also include *chromium* and *nightsurf* for web browsing.
 # - we include *amfora* for browsing the Gemini network.
+# - we have *xautolock* that runs *i3lock* after 10 mins of system inactivity. 
 ###############################
 i3_CONFIG_1_LOCATION="/etc/i3/config"
 i3_CONFIG_2_LOCATION="$HOME/.config/i3/config"
@@ -67,7 +68,7 @@ function i3_install() {		# this function installs vanilla i3
 	sudo pacman -S i3-wm
 }
 function wired_packs() {	# this function intalls the needed deps for wiredWM
-	sudo pacman -S git nitrogen arandr fish flameshot alacritty micro dunst amfora kitty feh code network-manager-applet conky vim sddm nnn picom chromium nemo
+	sudo pacman -S git nitrogen arandr fish xautolock flameshot alacritty micro dunst amfora kitty feh code network-manager-applet conky vim sddm nnn picom chromium nemo
 }
 function enable_AUR() {		# builds yay for using the AUR
 	echo "Enabling the Arch User Repository (AUR)..."
