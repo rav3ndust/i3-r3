70a5aa94e4b48e4151cd534decf5460178e34df3
rav3ndust <35274771+rav3ndust@users.noreply.github.com>
2022-11-23 18:01:45 -0600

    xterm -> pkgs

--- scripts-config/arch-install-demo/installer.sh
+++ scripts-config/arch-install-demo/installer.sh
@@ -74,7 +74,7 @@ function i3_install() {		# this function installs vanilla i3
 	sudo pacman -S i3-wm i3status
 }
 function wired_packs() {	# this function intalls the needed deps for wiredWM
-	sudo pacman -S git nitrogen lxsession arandr xscreensaver fish xorg-xkill xautolock flameshot alacritty micro dunst amfora kitty feh code network-manager-applet conky vim sddm nnn picom chromium nemo volumeicon
+	sudo pacman -S git nitrogen lxsession arandr xterm xscreensaver fish xorg-xkill xautolock flameshot alacritty micro dunst amfora kitty feh code network-manager-applet conky vim sddm nnn picom chromium nemo volumeicon
 }
 function enable_AUR() {		# builds yay for using the AUR
 	echo "Enabling the Arch User Repository (AUR)..."
