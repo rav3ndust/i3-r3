aa86206009fa82ca06809f7d585d344a9b32b49d
rav3ndust <35274771+rav3ndust@users.noreply.github.com>
2023-01-29 03:05:56 -0600

    add amfora into package list

--- scripts-config/installer.sh
+++ scripts-config/installer.sh
@@ -58,7 +58,7 @@ function i3_install() {		# this function installs vanilla i3 and i3lock-fancy
 	sudo apt-get install i3 i3lock-fancy -y
 }
 function wired_packs() {	# this function intalls the needed deps for wiredWM
-	sudo apt-get install nitrogen xterm arandr rofi xss-lock feh volumeicon-alsa polybar flameshot pasystray ffmpeg kitty stterm surf conky suckless-tools lxpolkit vim nnn cmus xscreensaver -y
+	sudo apt-get install nitrogen xterm arandr rofi xss-lock feh volumeicon-alsa polybar flameshot pasystray ffmpeg kitty stterm surf conky suckless-tools lxpolkit vim nnn cmus xscreensaver amfora -y
 }
 # script begins here
 # update system first and install needed wiredWM packages.
@@ -112,7 +112,7 @@ echo "Vim configs copied. You can change it at ~/.vimrc."
 # set the default background image.
 # we are going to use wp/nExt.png.
 echo "Setting default wallpaper..." && sleep 1
-nitrogen set-scaled $DEF_WP_LOCATION
+nitrogen --set-scaled $DEF_WP_LOCATION
 echo "Wallpaper saved. To change it, simply launch Nitrogen and choose whatever you would like." && sleep 1 
 # all done 
 echo "wiredWM has been installed." && sleep 1
