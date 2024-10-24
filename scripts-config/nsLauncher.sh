7c683baec8003a6ed9a062147dd20c117b5f7376
rav3ndust <35274771+rav3ndust@users.noreply.github.com>
2024-10-24 05:57:42 +0000

    Delete scripts-config/nsLauncher.sh

--- scripts-config/nsLauncher.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# a customized dmenu launcher that shows a list of applications with a .desktop entry
-#
-# used as an nsBlock in nightshadeWM
-i3-dmenu-desktop --dmenu='dmenu -i -nb black -nf purple -sb green -sf red -l 10 -p "nightshader - Applications List:"'
